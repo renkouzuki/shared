615bf909105028f7ebe29820f1a3a56f6c4aa3d1
Haichhan <ppfreea1@gmail.com>
2024-11-13 19:47:24 +0700

    updates

--- src/index.ts
+++ src/index.ts
@@ -1,73 +1,83 @@
 export {
-    IAuth,
-    IAuthPayload,
-    IAuthResponse,
-    IAuthDocument,
-    IAuthBuyerMessageDetails,
-    IEmailMessageDetails,
-    ISignUpPayload,
-    ISignInPayload,
-    IForgotPassword,
-    IResetPassword,
-    IReduxAuthPayload,
-    IReduxAddAuthUser,
-    IReduxLogout,
-    IAuthUser
+  IAuth,
+  IAuthPayload,
+  IAuthResponse,
+  IAuthDocument,
+  IAuthBuyerMessageDetails,
+  IEmailMessageDetails,
+  ISignUpPayload,
+  ISignInPayload,
+  IForgotPassword,
+  IResetPassword,
+  IReduxAuthPayload,
+  IReduxAddAuthUser,
+  IReduxLogout,
+  IAuthUser,
 } from './interfaces/auth.interface';
 
-export {
-    IBuyerDocument,
-    IReduxBuyer
-} from './interfaces/buyer.interface';
+export { IBuyerDocument, IReduxBuyer } from './interfaces/buyer.interface';
+
+export { SellerType, ILanguage } from './interfaces/seller.interface';
+
+export { GigType, ICreateGig } from './interfaces/gig.interface';
 
 export {
-    SellerType,
-    ILanguage
-} from './interfaces/seller.interface';
+  IConversationDocument,
+  IChatBoxProps,
+  IChatBuyerProps,
+  IChatMessageProps,
+  IChatSellerProps,
+  IMessageDetails,
+  IMessageDocument,
+} from './interfaces/chat.interface';
 
 export {
-    GigType,
-    ICreateGig
-} from './interfaces/gig.interface'
+  IHitsTotal,
+  IPaginateProps,
+  IQueryList,
+  IQueryString,
+  ISearchResult,
+  ITerm,
+} from './interfaces/search.interface';
 
 export {
-    IConversationDocument,
-    IChatBoxProps,
-    IChatBuyerProps,
-    IChatMessageProps,
-    IChatSellerProps,
-    IMessageDetails,
-    IMessageDocument,
-} from './interfaces/chat.interface'
+  IDeliveredWork,
+  IExtendedDelivery,
+  IOffer,
+  IOrderDocument,
+  IOrderEvents,
+  IOrderMessage,
+  IOrderNotifcation,
+  IOrderReview,
+} from './interfaces/order.interface';
+
+export { IEmailLocals } from './interfaces/email.interface';
 
 export {
-    IHitsTotal,
-    IPaginateProps,
-    IQueryList,
-    IQueryString,
-    ISearchResult,
-    ITerm
-} from './interfaces/search.interface'
-
+  IRatingCategories,
+  IRatingCategoryItem,
+  IRatingTypes,
+  IReviewDocument,
+  IReviewMessageDetails,
+} from './interfaces/review.interface';
+export { uploads, videoUploads } from './cloudinary-upload';
 export {
-    IDeliveredWork,
-    IExtendedDelivery,
-    IOffer,
-    IOrderDocument,
-    IOrderEvents,
-    IOrderMessage,
-    IOrderNotifcation,
-    IOrderReview
-} from './interfaces/order.interface'
-
+  IError,
+  IErrorResponse,
+  CustomError,
+  BadRequestError,
+  NotFoundError,
+  NotAuthorizedError,
+  FIleTooLargeError,
+  ServerError,
+  ErrnoException,
+} from './error-handler';
+export { verifyGatewayRequest } from './gateway-middleware';
+export { winstonLogger } from './logger';
 export {
-    IEmailLocals,
-} from './interfaces/email.interface'
-
-export {
-    IRatingCategories,
-    IRatingCategoryItem,
-    IRatingTypes,
-    IReviewDocument,
-    IReviewMessageDetails
-} from './interfaces/review.interface'
+  firstLetterUppercase,
+  lowerCase,
+  toUpperCase,
+  isEmail,
+  isDataURL,
+} from './helpers';
